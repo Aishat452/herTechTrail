62a3ab8b030b3926422f55c424e0713ce0698d40
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-12-28 12:15:31 +0100

    made changes to the paragraph tag when double clicked

--- first_assignment/index.js
+++ first_assignment/index.js
@@ -1,10 +1,18 @@
 const textChanges = document.querySelectorAll('p');
-
 textChanges.forEach((changeTextStyle) => {
     changeTextStyle.addEventListener('click', (e) => {
         e.target.style.backgroundColor = 'aqua';
         e.target.style.color = 'brown';
-        changeTextStyle.innerHTML = 'You\'ve Clicked';
+        changeTextStyle.innerHTML = 'You\'ve Clicked. Kindly double click to clear';
     })
 })
 
+
+const change_back = document.querySelectorAll('p');
+change_back.forEach((return_back)=>{
+    return_back.addEventListener('dblclick', (e)=>{
+        return_back.innerHTML = 'Cleared';
+        e.target.style.color = 'white';
+        e.target.style.backgroundColor = 'black';
+    })
+})
